5260d7d27876993da4a8d05bafed010280b91dcd
Theeeus <matheushsalves@gmail.com>
2017-02-02 13:07:36 -0200

    change fb callback

--- config/auth.js
+++ config/auth.js
@@ -3,7 +3,7 @@ module.exports = {
     'facebookAuth' : {
         'clientID'      : '1714422175537322', // your App ID
         'clientSecret'  : '1938d0f58689183e97f553a92182ff38', // your App Secret
-        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+        'callbackURL'   : 'http://murmuring-cove-35559.herokuapp.com/auth/facebook/callback'
     }
 
 };
